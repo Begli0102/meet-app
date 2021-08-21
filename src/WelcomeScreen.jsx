a2abb4963262fcac53ed218daf85b801939f8fd9
Begli0102 <begli.kaka@mail.ru>
2021-08-21 16:29:37 +0200

    add className

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -11,10 +11,9 @@ return props.showWelcomeScreen ?
 Log in to see upcoming events around the world for full-stack developers
 </h4>
 <div className="button_cont" align="center">
-<div class="google-btn">
-<div class="google-icon-wrapper">
-<img
-class="google-icon"
+<div className="google-btn">
+<div className="google-icon-wrapper">
+<img className="google-icon"
 src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
 o.svg"
 alt="Google sign-in"
@@ -22,7 +21,7 @@ alt="Google sign-in"
 </div>
 <button onClick={() => { props.getAccessToken() }}
 rel="nofollow noopener"
-class="btn-text"
+className="btn-text"
 >
 <b>Sign in with google</b>
 </button>
